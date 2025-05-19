5510831743b7fd4559c7bd4f7d125328cad9e82b
seonghunlee94 <godloveme94@naver.com>
2025-05-19 21:22:53 +0900

    test: add validation error handling for missing product description

--- test/integragtion/products.int.test.js
+++ test/integragtion/products.int.test.js
@@ -13,3 +13,12 @@ it('POST /api/products', async () => {
         
 })
 
+it("should return 500 on POST /api/products", async () => {
+    const response = await request(app)
+        .post('/api/products')
+        .send({ name: "test" });
+        
+        expect(response.statusCode).toBe(500);
+
+        expect(response.body).toStrictEqual({ message : "Product validation failed: description: Path `description` is required." })
+})
