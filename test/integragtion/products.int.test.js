56b2f66a7bee502f19b756e1908281f67eb9cbbc
seonghunlee94 <godloveme94@naver.com>
2025-05-25 19:30:35 +0900

    feat: add DELETE endpoint for product deletion and corresponding integration tests

--- test/integragtion/products.int.test.js
+++ test/integragtion/products.int.test.js
@@ -67,3 +67,18 @@ it("should return 404 on PUT /api/products", async () => {
     expect(res.statusCode).toBe(404);
 })
 
+it('DELETE /api/products/:productId', async () => {
+    const res = await request(app)
+        .delete('/api/products/' + firstProduct._id)
+        .send();
+        
+    expect(res.statusCode).toBe(200);
+})
+
+it("DELETE id doenst exist /api/products/:productId", async () => {
+    const res = await request(app)
+        .delete('/api/products/' + firstProduct._id)
+        .send();
+
+    expect(res.statusCode).toBe(404);
+})
